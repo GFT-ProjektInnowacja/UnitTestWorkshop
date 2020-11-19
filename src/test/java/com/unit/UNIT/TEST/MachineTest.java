d76512db6186eb4cd5fa01b31db6d15442fbd18e
mattu <mat.turkowski@gmail.com>
2020-11-19 13:45:10 +0100

    Tests from Mateusz T (sorry for sending this on Wednesday)

--- src/test/java/com/unit/UNIT/TEST/MachineTest.java
+++ src/test/java/com/unit/UNIT/TEST/MachineTest.java
@@ -1,6 +1,5 @@
 package com.unit.UNIT.TEST;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,61 +50,93 @@ class MachineTest {
     }
 
     @Test
-    public void shouldClearAmountOfMoney() {
+    public void shouldPlayerWinTheGame() {
         //given
-        int finalResult = 0;
-        //when
-        machine.winInfo();
-        //then
-        assertEquals(machine.bankAmount, finalResult);
-    }
+        List<Integer> integerList = new ArrayList<>();
 
-    @Test
-    public void shouldFiveElementsInList() {
-        //given
-        int numberOfElements = 5;
-        //when
-        machine.generateThreeSigns();
-        //then
-        assertEquals(machine.gameSigns.size(), numberOfElements);
-    }
+        for (int i = 0; i < 5; i++)
+            integerList.add(7);
 
-    @Test
-    public void shouldElementsFromZeroToNine() {
-        //given
-        CheckNumberTest checkTest = new CheckNumberTest();
-        //when
-        machine.generateThreeSigns();
-        //then
-        assertTrue(checkTest.check(machine.gameSigns));
-    }
+        machine.gameSigns = integerList;
 
-    @Test
-    public void shouldPlayerWin() {
-        //given
-        List<Integer> mockList = new ArrayList<>();
-        mockList.add(7);
-        mockList.add(7);
-        mockList.add(7);
-        mockList.add(7);
-        mockList.add(7);
-        machine.gameSigns = mockList;
         //when
         machine.checkWin();
+
         //then
         assertTrue(machine.checkWin());
     }
 
+
     @Test
-    public void shouldNotIncreaseAmountOfMoney() {
+    public void shouldPlayerLoseTheGame() {
         //given
-        Money money = new Money(10);
-        int finalResult = machine.bankAmount;
-        when(validator.validateMoney(money)).thenReturn(false);
+        List<Integer> integerList = new ArrayList<>();
+
+        for(int i = 1; i < 6; i++)
+            integerList.add(i);
+
+        machine.gameSigns = integerList;
+
         //when
-        machine.insertCoin(money);
+        machine.checkWin();
+
+        //then
+        assertFalse(machine.checkWin());
+    }
+
+    @Test
+    public void shouldIncreaseListOfAmountOfMoney() {
+        //given
+        List <Money> moneyList = new ArrayList<>();
+
+        int finalBankAccountResult = 10025;
+
+        for (int i = 0; i < 5; i++)
+            moneyList.add(new Money(5));
+
+        for (Money money : moneyList)
+            when(validator.validateMoney(money)).thenReturn(true);
+
+        //when
+        for (Money money : moneyList)
+            machine.insertCoin(money);
+
+        //then
+        assertEquals(finalBankAccountResult, machine.bankAmount);
+        verify(validator,times(5)).validateMoney(any(Money.class));
+    }
+
+    @Test
+    public void shouldNotIncreaseListOfAmountOfMoney() {
+        //given
+        List<Money> moneyList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++)
+            moneyList.add(new Money(50));
+
+        int finalResult = machine.bankAmount;
+
+        for (Money money : moneyList)
+            when(validator.validateMoney(money)).thenReturn(false);
+
+        //when
+        for (Money money : moneyList)
+            machine.insertCoin(money);
+
         //then
         assertEquals(finalResult, machine.bankAmount);
-        verify(validator,times(1)).validateMoney(money);
+        verify(validator,times(5)).validateMoney(any(Money.class));
+    }
+
+    @Test
+    public void shouldClearBankAccountAfterPlayerWin() {
+        //given
+        int accountAfterWin = 0;
+
+        //when
+        machine.winInfo();
+
+        //then
+        assertEquals(accountAfterWin, machine.bankAmount);
     }
 }
